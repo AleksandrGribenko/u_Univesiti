39d872507d905ad4e3ca21ba75531de7444a8739
AleksandrGribenko <49722929+AleksandrGribenko@users.noreply.github.com>
2024-12-11 14:18:18 +0500

    Add files via upload
    Задание

---
+++ Example_1/Example.kt
@@ -0,0 +1,14 @@
+package Example_1
+
+
+fun main() {
+    val age: Int
+    age = 48
+    val name: String
+    name = "Александр"
+    val isStudent: Boolean
+    isStudent = true
+    val salary: Double
+    salary = 10000.0
+    println("Age: " + age + " | Name: " + name + " | IsStudent: " + isStudent + " | Salary: " + salary)
+}
