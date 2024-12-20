2c92ae7c9cdfa0718e125584127fb51e2a745bc5
AleksandrGribenko <49722929+AleksandrGribenko@users.noreply.github.com>
2024-12-20 05:53:45 +0500

    Add files via upload
    Исправленный

---
+++ Example_8/Ex_8.kt
@@ -0,0 +1,43 @@
+package Example_8
+
+import java.lang.Math.pow
+import kotlin.math.roundToInt
+
+fun main() {
+//Создать массив из 10 чисел с плавающей точкой. Найти произведение его элементов.
+    val array = arrayOf<Double>(1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1, 10.5)
+    var mult: Double = 1.0
+    for (i in array) {
+        mult *= i
+    }
+    println("%1.5f".format(mult))
+
+    //Создать массив из 8 целых чисел. Найти среднее арифметическое его элементов.
+    val array2 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
+    val average = array2.average()
+    println(average)
+
+    //Дан массив размерностью size. Вывести в том же порядке все четные числа из данного набора и количество K таких чисел.
+    val array3 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    var columns = 0
+    for (i in 0 until array3.size) {
+        if (array3[i] % 2 == 0){
+            print("${array3[i]} ")
+            columns++
+        }
+
+    }
+    println()
+    println(columns)
+
+//Дано целое число N(>0).
+// Сформировать и вывести целочисленный массив размера N, содержащий степени двойки от первой до N-й: 2,4,8,16,….
+    println("Введите размер массива")
+    var n = readln().toInt()
+    val array4 = IntArray(n)
+    for (index in 0..n - 1) {
+
+        array4[index] = pow(2.0, index.toDouble()).toInt()
+    }
+    println(array4.contentToString())
+}
